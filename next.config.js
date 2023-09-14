83b26f7fa34ac266ac86abe3eaec2ff44333a220
rizkyalvie <rizkyalviee@gmail.com>
2023-09-14 21:05:55 +0700

    No lint build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'i.pinimg.com',
-            port: '',
-            pathname: '/564x/93/aa/bb/93aabb88ca7c842bc019fe695a84b981.jpg',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        port: "",
+        pathname: "/564x/93/aa/bb/93aabb88ca7c842bc019fe695a84b981.jpg",
       },
-}
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
